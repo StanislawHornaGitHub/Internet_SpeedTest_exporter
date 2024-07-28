92f349f836bf38fa825f2bbe8d9e79d7a42171c7
Stanislaw Horna <stanislawhorna@outlook.com>
2024-07-28 23:23:30 +0200

    remove deprecated @app.on_event(startup)

--- app/main.py
+++ app/main.py
@@ -1,26 +1,25 @@
 from fastapi import FastAPI
-from prometheus_client import make_asgi_app
-from src.Controller.SpeedTest import SpeedTest
-import src.Model as Model
+from contextlib import asynccontextmanager
 from fastapi_utils.tasks import repeat_every
+from prometheus_client import make_asgi_app
+import src.Controller as Controller
+
 metrics_app = make_asgi_app()
 
-app = FastAPI()
+
+@repeat_every(seconds=60 * 60)
+def perform_speed_test():
+    Controller.SpeedTest.perform_speed_test()
+    print("test performed")
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    Controller.SpeedTest.check_components()
+    await perform_speed_test()
+    yield
+    print("Closing app")
+
+app = FastAPI(lifespan=app_lifespan)
 
 app.mount("/metrics", metrics_app)
-
-# @app.route("/metrics")
-# def update_results(temp):
-#     SpeedTest.perform_speed_test()
-
-#     return metrics_app
-
-
-@repeat_every(seconds=10)
-def perform_speed_test():
-    SpeedTest.perform_speed_test()
-    print("test performed")
-    
-@app.on_event("startup")
-async def app_startup():
-    await perform_speed_test()

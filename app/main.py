b20f91a7896e0df721bd005b73170285dd94f919
Stanisław Horna <stanislawhorna@outlook.com>
2024-07-28 21:10:21 +0200

    add scheduled task to perform test and update metrics instead of doing it during prometheus request

--- app/main.py
+++ app/main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI
-from prometheus_client import make_asgi_app, Gauge
-
-from src.Model.SpeedTest import SpeedTest
-from src.Utils.SpeedTestGauges import SpeedTestGauges
-
+from prometheus_client import make_asgi_app
+from src.Controller.SpeedTest import SpeedTest
+import src.Model as Model
+from fastapi_utils.tasks import repeat_every
 metrics_app = make_asgi_app()
 
 app = FastAPI()
 
+app.mount("/metrics", metrics_app)
 
-@app.route("/metrics")
-def update_results(temp):
-    print(temp)
-    SpeedTestGauges.isp.info({"isp": "Play"
-    })
+# @app.route("/metrics")
+# def update_results(temp):
+#     SpeedTest.perform_speed_test()
 
-    return metrics_app
+#     return metrics_app
+
+
+@repeat_every(seconds=10)
+def perform_speed_test():
+    SpeedTest.perform_speed_test()
+    print("test performed")
+    
+@app.on_event("startup")
+async def app_startup():
+    await perform_speed_test()
